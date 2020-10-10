37e0ea8eb2d705484941f6c092c80a0c60752210
Sardar Hamza Naqi <sardarhamzanaqi@gmail.com>
2020-10-10 18:31:08 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -11,6 +11,10 @@ classs CreateArtists < ActiveRecord::Migration[5.2]
   
   def change
     create_table :artists do |t|
-  end
+      t.string :name
+      t.string :genre
+      t.integer :age
+      t.string :hometown
+    end
   end
 end
